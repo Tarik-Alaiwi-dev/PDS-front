41f95357dd5c80ae97832c859afeb2c9c2c5321a
Tarik Alaiwi <tarik.alaiwi.dev@gmail.com>
2024-09-16 18:42:44 +0200

    add files for deployment

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<template>
+
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand p-1">
+        <router-link to="/" class="navbar-item"><strong>Pneumonia Detection System</strong></router-link>
+
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{ 'is-active': showMobileMenu }">
+        <div class="navbar-end">
+          <router-link to="/log-out" class="button is-danger m-3"><span> <i class="fa-solid fa-right-from-bracket"></i> Log Out</span></router-link>
+        </div>
+      </div>
+    </nav>
+
+    <div class="is-loading-bar has-text-centered" v-bind:class="{ 'is-loading': $store.state.isLoading }">
+      <div class="lds-dual-ring"></div>
+    </div>
+
+    <section class="section">
+      <router-view/>
+    </section>
+
+    <footer class="footer">
+      <p class="has-text-centered">Copyright (c) 2024</p>
+    </footer>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMobileMenu: false,
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../node_modules/bulma';
+
+.lds-dual-ring {
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+.lds-dual-ring:after {
+  content: " ";
+  display: block;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6px solid #ccc;
+  border-color: #ccc transparent #ccc transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.is-loading-bar {
+  height: 0;
+  overflow: hidden;
+
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
+  
+  &.is-loading {
+    height: 80px;
+  }
+}
+</style>
